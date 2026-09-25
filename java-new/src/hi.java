b42da1bc7cab2c5d155714aad9ba363ceaae71f7
최준혁 <leisure1566@gmail.com>
2026-09-25 10:14:16 +0000

    Variable

--- java-new/src/hi.java
+++ java-new/src/hi.java
@@ -1,6 +1,8 @@
 public class hi {
     public static void main(String[] args) {
-        System.out.println("hello java1"); //hello java1을 출력
+        System.out.println("hello java1"); //hello java1을 출력 한줄 주석
+        /* 이건
+        여러줄 주석이노 */
     }
     
 }
---
+++ java-new/src/variable/Var1.java
@@ -0,0 +1,10 @@
+public class Var1{
+    public static void main(String[] args) {
+        int a;
+        a = 20;
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+    }
+
+}

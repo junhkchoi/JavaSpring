fa3ec472fd9b5cdf95dd5ae8f4417958acb5b8cf
최준혁 <leisure1566@gmail.com>
2026-09-25 07:41:53 +0000

    m

---
+++ java-new/src/HelloJava2.java
@@ -0,0 +1,7 @@
+public class HelloJava2 {
+    public static void main(String[] args) {
+        System.out.println("hi1");
+        System.out.println("hi2");
+        System.out.println("hi3");
+    }
+}
---
+++ java-new/src/hi.java
@@ -0,0 +1,6 @@
+public class hi {
+    public static void main(String[] args) {
+        System.out.println("hello java1"); //hello java1을 출력
+    }
+    
+}
